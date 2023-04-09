5dce61a3deaaa99ece7cb4a9e346a5f9773e267a
Ian Qvist <qvistian@gmail.com>
2023-04-09 19:18:56 +0200

    Add tests for transforms

--- Src/EnumSourceGen.Tests.Functionality/Code/Enums.cs
+++ Src/EnumSourceGen.Tests.Functionality/Code/Enums.cs
@@ -21,6 +21,25 @@ namespace Genbox.EnumSourceGen.Tests.Functionality.Code
         Other = 256,
         Min = long.MinValue
     }
+
+    [EnumSourceGen]
+    public enum TestTransformsEnum : long
+    {
+        [EnumConfig(NameOverride = "ThisWasOverriden")]
+        OverrideMe,
+        [EnumConfig(SimpleTransform = EnumTransform.UpperCase)]
+        UpperCase,
+        [EnumConfig(SimpleTransform = EnumTransform.LowerCase)]
+        LowerCase,
+        [EnumConfig(AdvancedTransform = "ULULU____O")]
+        MixedCase1,
+        [EnumConfig(AdvancedTransform = "/1//")]
+        R1e1m1o1v1e1A1l1l1O1n1e1s,
+        [EnumConfig(Omit = true)]
+        Omitted,
+        [EnumConfig(Omit = true, OmitExclude = EnumOmitExclude.GetString)]
+        OmittedWithFilter
+    }
 }
 
 namespace Genbox.EnumSourceGen.Tests.Functionality.OtherNamespace
---
+++ Src/EnumSourceGen.Tests.Functionality/EnumTransformTests.cs
@@ -0,0 +1,18 @@
+﻿using Genbox.EnumSourceGen.Tests.Functionality.Code;
+
+namespace Genbox.EnumSourceGen.Tests.Functionality;
+
+public class EnumTransformTests
+{
+    [Fact]
+    public void TransformTest()
+    {
+        Assert.Equal("ThisWasOverriden", TestTransformsEnum.OverrideMe.GetString());
+        Assert.Equal("UPPERCASE", TestTransformsEnum.UpperCase.GetString());
+        Assert.Equal("lowercase", TestTransformsEnum.LowerCase.GetString());
+        Assert.Equal("MiXeDCase", TestTransformsEnum.MixedCase1.GetString());
+        Assert.Equal("RemoveAllOnes", TestTransformsEnum.R1e1m1o1v1e1A1l1l1O1n1e1s.GetString());
+        Assert.Equal("", TestTransformsEnum.Omitted.GetString());
+        Assert.Equal("OmittedWithFilter", TestTransformsEnum.OmittedWithFilter.GetString());
+    }
+}
