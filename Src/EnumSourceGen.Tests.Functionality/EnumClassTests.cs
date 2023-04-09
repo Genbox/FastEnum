dd010cf8ed5464e0333a1782eb2d56f08be4195e
Ian Qvist <qvistian@gmail.com>
2023-04-09 19:08:04 +0200

    Refactor tests

--- Src/EnumSourceGen.Tests.Functionality/EnumClassTests.cs
+++ Src/EnumSourceGen.Tests.Functionality/EnumClassTests.cs
@@ -2,81 +2,8 @@ using Genbox.EnumSourceGen.Tests.Functionality.Code;
 
 namespace Genbox.EnumSourceGen.Tests.Functionality;
 
-public class FunctionalityTests
+public class EnumClassTests
 {
-    private const TestEnum _valid = TestEnum.First;
-    private const TestEnum _invalid = (TestEnum)100;
-
-    [Fact]
-    public void GetStringTest()
-    {
-        Assert.Equal("First", _valid.GetString());
-    }
-
-    [Fact]
-    public void GetUnderlyingValueTest()
-    {
-        Assert.True(_valid.TryGetUnderlyingValue(out long underlyingValue));
-        Assert.Equal(8, underlyingValue);
-
-        Assert.False(_invalid.TryGetUnderlyingValue(out underlyingValue));
-
-        Assert.Equal(8, _valid.GetUnderlyingValue());
-        Assert.Throws<ArgumentOutOfRangeException>(() => _invalid.GetUnderlyingValue());
-    }
-
-    [Fact]
-    public void GetDisplayNameTest()
-    {
-        Assert.True(_valid.TryGetDisplayName(out string? displayName));
-        Assert.Equal("FirstDisplayName", displayName);
-
-        Assert.False(_invalid.TryGetDisplayName(out displayName));
-
-        Assert.Equal("FirstDisplayName", _valid.GetDisplayName());
-        Assert.Throws<ArgumentOutOfRangeException>(() => _invalid.GetDisplayName());
-    }
-
-    [Fact]
-    public void GetDescriptionTest()
-    {
-        Assert.True(_valid.TryGetDescription(out string? description));
-        Assert.Equal("FirstDescription", description);
-
-        Assert.False(_invalid.TryGetDescription(out description));
-
-        Assert.Equal("FirstDescription", _valid.GetDescription());
-        Assert.Throws<ArgumentOutOfRangeException>(() => _invalid.GetDescription());
-    }
-
-    [Fact]
-    public void IsFlagSetTest()
-    {
-        TestEnum e = TestEnum.First;
-        Assert.True(e.IsFlagSet(TestEnum.First));
-
-        e = TestEnum.Second;
-        Assert.False(e.IsFlagSet(TestEnum.First));
-
-        e = TestEnum.First | TestEnum.Second | TestEnum.Third;
-        Assert.True(e.IsFlagSet(TestEnum.First));
-        Assert.True(e.IsFlagSet(TestEnum.Second));
-        Assert.True(e.IsFlagSet(TestEnum.Third));
-
-        //Exhaustive test against dotnet's HasFlag(). 8 is the highest value of TestEnum.
-        for (int i = 0; i < 256; i++)
-        {
-            TestEnum value = (TestEnum)i;
-
-            for (int j = 0; j < 8; j++)
-            {
-                TestEnum flags = (TestEnum)j;
-
-                Assert.Equal(value.HasFlag(flags), value.IsFlagSet(flags));
-            }
-        }
-    }
-
     [Fact]
     public void MemberCountTest()
     {
@@ -126,7 +53,7 @@ public class FunctionalityTests
         Assert.True(Enums.TestEnum.IsDefined(TestEnum.First | TestEnum.Second | TestEnum.Third));
         Assert.True(Enums.TestEnum.IsDefined(TestEnum.First | TestEnum.Second));
         Assert.True(Enums.TestEnum.IsDefined(TestEnum.First));
-        Assert.False(Enums.TestEnum.IsDefined(_invalid));
+        Assert.False(Enums.TestEnum.IsDefined((TestEnum)100));
 
         //We also explicitly test a non flags enum
         Assert.True(Enums.NonFlagsEnum.IsDefined(NonFlagsEnum.Value1));
